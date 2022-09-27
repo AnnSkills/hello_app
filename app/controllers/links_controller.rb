5b6daca28c26a62ced3f7085ba76e66a889ccf5b
Anna <khomiakova25@gmail.com>
2022-09-27 14:35:53 +0300

    update operations created

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,4 +2,41 @@ class LinksController < ApplicationController
   def show
     @link = Link.find(params[:id])
   end
+
+  def index
+    @links = Link.all
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+  def create
+    @link = Link.new(params.require(:link).permit(:link, :description))
+    if @link.save
+      flash[:notice] = "Link was created successfully."
+    redirect_to @link
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(params.require(:link).permit(:link, :description))
+      flash[:notice] = "Link edited"
+      redirect_to @link
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+    redirect_to links_path
+  end
 end
