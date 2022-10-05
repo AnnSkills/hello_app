a3ddb8fb57af4d0c5bcd4bd9b6e8859ae93e1669
Anna <khomiakova25@gmail.com>
2022-10-05 15:17:05 +0300

    Creating process of category

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, except: [:new]
   resources :links
+  resources :categories, except: [:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
